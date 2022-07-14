f7d7a9870a7a7f186c076a584ea733cfdc518012
Gabriel Lustosa Queiroz <lustosaki2@gmail.com>
2022-07-14 16:05:47 -0300

    DEPLOY[DEPLOY]: DEPLOY

--- flashcards/apps/user/views.py
+++ flashcards/apps/user/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login
+from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -10,6 +11,9 @@ class UserRegisterView(CreateView):
     template_name = 'registration/register.html'
     form_class = UserCreateForm
     success_url = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        raise PermissionDenied()
 
     def form_valid(self, form):
         result = super().form_valid(form)
