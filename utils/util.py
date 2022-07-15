0bbaec7a4d8a9e82370b7a79d5a59f831ab726ac
Gabriel Lustosa Queiroz <lustosaki2@gmail.com>
2022-07-15 13:14:39 -0300

    FIX[WORD]: Arrumando replaced_string example close word

--- utils/util.py
+++ utils/util.py
@@ -11,43 +11,28 @@ def shuffle_from_dict(d):
 
 def escape(word, sentence):
     sentence_lower = sentence.lower()
-    word = word.lower()
-    if ' ' in word:
-        word = word.split(' ')
+    word = word.lower().split(' ')
 
     new_sentence = ''
     for index, word_sentence in enumerate(sentence_lower.split(' ')):
         new_word = sentence.split(' ')[index]
         word_sentence = word_sentence.translate(str.maketrans('', '', string.punctuation))
-        if isinstance(word, list):
-            for word_part in word:
-                if word_part == word_sentence:
-                    replaced_string = ''.join(['_' for n in range(len(word_part))])
-                    new_word = replaced_string
-        else:
-            replaced_string = ''.join(['_' for n in range(len(word))])
-            if word == word_sentence:
+        for word_part in word:
+            if word_part == word_sentence:
+                replaced_string = ''.join(['_' for n in range(len(word_part))])
                 new_word = replaced_string
         new_sentence += new_word
         new_sentence += ' '
 
-    if isinstance(word, list):
-        close_matches = []
-        for word_part in word:
-            matches = difflib.get_close_matches(word_part, new_sentence.split(' '))
-            close_matches.extend(matches)
-    else:
-        close_matches = difflib.get_close_matches(word, new_sentence.split(' '))
+    close_matches = []
+    for word_part in word:
+        matches = difflib.get_close_matches(word_part, new_sentence.split(' '))
+        close_matches.extend(matches)
     if close_matches:
         for match in close_matches:
             match_str = str(match).lower()
-            if isinstance(word, list):
-                for word_part in word:
-                    if match_str.startswith(word_part):
-                        replaced_string = ''.join(['_' for n in range(len(match_str))])
-                        new_sentence = new_sentence.replace(match_str, replaced_string)
-            else:
-                if match_str.startswith(word):
+            for word_part in word:
+                if match_str.startswith(word_part):
                     replaced_string = ''.join(['_' for n in range(len(match_str))])
                     new_sentence = new_sentence.replace(match_str, replaced_string)
 
